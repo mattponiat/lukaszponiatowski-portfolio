6eac2858c489e86dd20844c76feea105ae171ee6
mattponiat <pinkyball50@gmail.com>
2022-04-20 21:53:28 +0200

    Add ContactList component

---
+++ components-ui/home/molecules/ContactList/index.tsx
@@ -0,0 +1,34 @@
+//Styled-components
+import styled from "styled-components";
+//Components
+import ContactIcon from "components-ui/home/atoms/ContactIcon";
+
+const ContactList = ({
+  icon,
+  text,
+}: {
+  icon: React.ReactElement;
+  text: string;
+}) => {
+  return (
+    <Wrapper>
+      <ContactIcon icon={icon} />
+      <StyledSpan>{text}</StyledSpan>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const StyledSpan = styled.span`
+  margin-left: 25px;
+  color: ${({ theme }) => theme.colors.text.homeHeading};
+  font-family: ${({ theme }) => theme.font.family[800]};
+  letter-spacing: 1px;
+`;
+
+export default ContactList;
