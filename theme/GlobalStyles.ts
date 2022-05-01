e2d90855c0f23b86b71d78737d29478f6f36f9be
mattponiat <pinkyball50@gmail.com>
2022-05-01 16:23:57 +0200

    Remove @font-face

--- theme/GlobalStyles.ts
+++ theme/GlobalStyles.ts
@@ -10,49 +10,7 @@ export default createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        font-family: 'Nunito Sans Regular', sans-serif;
+        font-family: 'Nunito Sans', sans-serif;
         line-height: 1.5;
-
-        @font-face {
-            font-family: 'Nunito Sans ExtraLight';
-            src: url('/fonts/NunitoSans/NunitoSans-ExtraLight.ttf');
-            font-display: swap;
-        }
-
-        @font-face {
-            font-family: 'Nunito Sans Light';
-            src: url('/fonts/NunitoSans/NunitoSans-Light.ttf');
-            font-display: swap;
-        }
-
-        @font-face {
-            font-family: 'Nunito Sans Regular';
-            src: url('/fonts/NunitoSans/NunitoSans-Regular.ttf');
-            font-display: swap;
-        }
-
-        @font-face {
-            font-family: 'Nunito Sans SemiBold';
-            src: url('/fonts/NunitoSans/NunitoSans-SemiBold.ttf');
-            font-display: swap;
-        }
-
-        @font-face {
-            font-family: 'Nunito Sans Bold';
-            src: url('/fonts/NunitoSans/NunitoSans-Bold.ttf');
-            font-display: swap;
-        }
-
-        @font-face {
-            font-family: 'Nunito Sans ExtraBold';
-            src: url('/fonts/NunitoSans/NunitoSans-ExtraBold.ttf');
-            font-display: swap;
-        }
-
-        @font-face {
-            font-family: 'Nunito Sans Black';
-            src: url('/fonts/NunitoSans/NunitoSans-Black.ttf');
-            font-display: swap;
-        }
     }
 `;
