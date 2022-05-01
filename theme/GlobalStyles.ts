a2780ab91d7987cea289f8e501c7c19824817d6d
mattponiat <pinkyball50@gmail.com>
2022-05-01 16:49:07 +0200

    Use font-family in *

--- theme/GlobalStyles.ts
+++ theme/GlobalStyles.ts
@@ -5,12 +5,12 @@ export default createGlobalStyle`
         box-sizing: border-box;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+        font-family: 'Nunito Sans', sans-serif;
     }
 
     body {
         margin: 0;
         padding: 0;
-        font-family: 'Nunito Sans', sans-serif;
         line-height: 1.5;
     }
 `;
