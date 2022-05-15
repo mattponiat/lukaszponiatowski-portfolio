7c59349f4d401c11ccc690abb116d2b3e3af041b
mattponiat <pinkyball50@gmail.com>
2022-05-15 17:26:11 +0200

    Update components exports

--- components/index.tsx
+++ components/index.tsx
@@ -1,1 +1,7 @@
 export * from "./Home";
+export * from "./Portfolio";
+export * from "./About";
+export * from "./Header";
+export * from "./Footer";
+export * from "./FooterLayout";
+export * from "./HomeLayout";
