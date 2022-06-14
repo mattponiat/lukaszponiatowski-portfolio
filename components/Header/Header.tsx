23c771822a167c14407df2c80975e141b6369d5f
Mateusz Poniatowski <pinkyball50@gmail.com>
2022-06-14 23:37:38 +0200

    Add phone layout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,94 @@
+import * as React from "react";
 //Styled-components
 import styled from "styled-components";
 //Components
-import { Navbar } from "components/Header/Navbar";
+import { Navbar } from "./Navbar";
+//Hooks
+import { useWindowSize } from "usehooks-ts";
+//MUI
+import Dialog from "@mui/material/Dialog";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
+import Fade from "@mui/material/Fade";
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Fade ref={ref} {...props} />;
+});
 
 const Header = () => {
+  const [open, setOpen] = React.useState(false);
+  const { width } = useWindowSize();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Wrapper>
-      <Navbar />
+      {width > 768 ? (
+        <Navbar />
+      ) : (
+        <>
+          <StyledButton onClick={handleOpen}>
+            <StyledHamburgerIcon />
+          </StyledButton>
+          <Dialog
+            fullScreen
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Transition}
+          >
+            <StyledDialogContent>
+              <IconButton
+                size="medium"
+                onClick={handleClose}
+                aria-label="close"
+                style={{
+                  alignSelf: "end",
+                  margin: "10px 0",
+                }}
+              >
+                <StyledCloseIcon fontSize="large" color="inherit" />
+              </IconButton>
+              <StyledNavWrapper>
+                <StyledLink onClick={handleClose} href="/">
+                  HOME
+                </StyledLink>
+                <StyledLink onClick={handleClose} href="/portfolio">
+                  PORTFOLIO
+                </StyledLink>
+                <StyledLink onClick={handleClose} href="/kontakt">
+                  KONTAKT
+                </StyledLink>
+              </StyledNavWrapper>
+            </StyledDialogContent>
+          </Dialog>
+        </>
+      )}
     </Wrapper>
+  );
+};
+
+const StyledHamburgerIcon = () => {
+  return (
+    <svg viewBox="0 0 10 8" width="37">
+      <path
+        d="M1 1h8M1 4h 8M1 7h8"
+        stroke="#333333"
+        strokeWidth="1.1"
+        strokeLinecap="round"
+      />
+    </svg>
   );
 };
 
@@ -20,6 +101,54 @@ const Wrapper = styled.header`
   min-height: 70px;
   background-color: ${({ theme }) => theme.colors.mainBg};
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+
+  @media screen and (max-width: 768px) {
+    justify-content: end;
+  }
+`;
+
+const StyledButton = styled.button`
+  display: grid;
+  place-items: center;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const StyledCloseIcon = styled(CloseIcon)`
+  color: ${({ theme }) => theme.colors.mainBg};
+`;
+
+const StyledDialogContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  max-width: 100%;
+  min-height: 100%;
+  padding: 0 10px;
+  background-color: ${({ theme }) => theme.colors.secondaryBg};
+  opacity: 0.99;
+`;
+
+const StyledNavWrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto 0 384.5px 0;
+`;
+
+const StyledLink = styled.a`
+  margin: 10px 0;
+  font-size: ${({ theme }) => theme.font.size.xlarge};
+  color: ${({ theme }) => theme.colors.mainBg};
+  letter-spacing: 1px;
+  text-decoration: none;
+  transition: color 0.3s;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.lightGrey};
+  }
 `;
 
 export { Header };
