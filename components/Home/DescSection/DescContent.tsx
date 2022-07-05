43ac12199d040758ad1581dbbe37fec3a99d477b
Mateusz Poniatowski <pinkyball50@gmail.com>
2022-07-05 18:43:42 +0200

    Update DescContent text

--- components/Home/DescSection/DescContent.tsx
+++ components/Home/DescSection/DescContent.tsx
@@ -5,24 +5,25 @@ const DescContent = () => {
   return (
     <Wrapper>
       <StyledText>
-        Nazywam się Łukasz Poniatowski, realizuje zlecenia fotograficzne na
-        terenie całej Polski. Dokumentuje wasze uroczystości oraz ważne
+        Nazywam się Łukasz Poniatowski, realizuję zlecenia fotograficzne na
+        terenie całej Polski. Dokumentuję Wasze uroczystości oraz ważne
         wydarzenia rodzinne. Prezentowana przeze mnie usługa charakteryzuje się
-        indywidualnym podejściem do klienta, przed realizacja zależy mi na
-        dokładnym zapoznaniu się z wizją i oczekiwaniami dotyczących
-        okoliczności. Reportaż traktuje jako opowieść o niepowtarzalnej narracji
-        i rytmie, emocjonującą historię która stanowi pamiątkę tych
+        indywidualnym podejściem do klienta. Przed realizacją zależy mi na
+        dokładnym zapoznaniu się z wizją i oczekiwaniami dotyczącymi danej
+        okoliczności. Reportaż traktuję jako opowieść o niepowtarzalnej narracji
+        i rytmie, emocjonującą historię, która stanowi pamiątkę tych
         fantastycznych wydarzeń na całe życie.
       </StyledText>
       <StyledText>
-        Wiem jak dużym jest to wyzwaniem oraz z jaką wiąże się
+        Wiem, jak dużym jest to wyzwaniem oraz z jaką wiąże się
         odpowiedzialnością. Dlatego jako fotograf jestem ogromnie wdzięczny za
-        zaufanie jakim darzą mnie moi klienci, zajmując się tym profesjonalnie
-        obowiązkiem jest zapewnienie pierwszorzędnych efektów. Oferuje
-        jutrzejsze spojrzenie na fotografie wraz z kreatywnym wykorzystaniem
+        zaufanie jakim darzą mnie moi klienci. Zajmując się tym profesjonalnie
+        moim obowiązkiem jest zapewnienie pierwszorzędnych efektów. Oferuję
+        jutrzejsze spojrzenie na fotografię wraz z kreatywnym wykorzystaniem
         roli post produkcji. Nie podążam utartymi szlakami i zależy mi na
         przygotowaniu dla Państwa unikatowych historii. Serdecznie zapraszam do
-        kontaktu gdzie przedstawię szczegóły oferty i odpowiem na każde pytanie.
+        kontaktu, gdzie przedstawię szczegóły oferty i odpowiem na każde
+        pytanie.
       </StyledText>
     </Wrapper>
   );
