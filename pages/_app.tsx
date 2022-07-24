25d52dd39f4ec2405de736e2910b886bf97c3f03
Mateusz Poniatowski <pinkyball50@gmail.com>
2022-07-24 17:39:29 +0200

    Update metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,26 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import theme from "theme/theme";
 import GlobalStyles from "theme/GlobalStyles";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {getLayout(<Component {...pageProps} />)}
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="author" content="Łukasz Poniatowski" />
+        <meta
+          name="keywords"
+          content="lukasz, łukasz, poniatowski, lukasz poniatowski, łukasz poniatowski, fotografia, photography, portfolio, photographer, fotograf"
+        />
+        <meta name="robots" content="index, follow" />
+        <title>Łukasz Poniatowski Photography</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </>
   );
 }
 
