09b62ccc9340eaa77dc361c0a9a20200abc516eb
Mateusz Poniatowski <pinkyball50@gmail.com>
2022-08-23 14:17:55 +0200

    Fix typos

--- components/Home/HomePage.tsx
+++ components/Home/HomePage.tsx
@@ -9,8 +9,8 @@ const HomePage = () => {
       <InfoSection
         titleSize={52}
         title="ŁUKASZ PONIATOWSKI"
-        quote="“Fotograf z wrażliwym podejściem do barw, oprawi wasze emocje w niebanalną kompozycję.”"
-        about="Witaj, bardzo się cieszę, że tutaj jesteś. Korzystając z okazji pragnę zaprosić Cię do zapoznania się z moją osobą, a także portfolio stworzonym specjalnie z myślą o Tobie. Wykonuje fotoreportaże z uroczystości okolicznościowych, jednak to nie wszystko co posiadam w swojej ofercie."
+        quote="“Fotograf z wrażliwym podejściem do barw, oprawi Wasze emocje w niebanalną kompozycję.”"
+        about="Witaj, bardzo się cieszę, że tutaj jesteś. Korzystając z okazji pragnę zaprosić Cię do zapoznania się z moją osobą, a także portfolio stworzonym specjalnie z myślą o Tobie. Wykonuje fotoreportaże z uroczystości okolicznościowych, jednak to nie wszystko, co posiadam w swojej ofercie."
       />
       <ImgCarouselSection />
       <DescSection />
